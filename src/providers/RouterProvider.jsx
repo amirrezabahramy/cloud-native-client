ccb250e2bc55efff2880655e5e133f2db9d1eb77
amirrezabahramy <amirrezabahramy@gmail.com>
2024-02-29 13:23:07 +0330

    Added routes for auth layouts

--- src/providers/RouterProvider.jsx
+++ src/providers/RouterProvider.jsx
@@ -1,8 +1,10 @@
 import {
   RouterProvider as TanstackRouterProvider,
+  lazyRouteComponent as lazy,
   Outlet,
   createRootRoute,
   createRouter,
+  createRoute,
 } from "@tanstack/react-router";
 import { useAuth } from "./AuthProvider";
 import { useEffect } from "react";
@@ -10,9 +12,14 @@ import PendingComponent from "@/components/generic/PendingComponent";
 import NotFoundComponent from "@/components/generic/NotFoundComponent";
 
 /*** Layouts ***/
+// Auth
+const AuthLayout = lazy(() => import("@/layouts/auth"));
+const AuthLoginLayout = lazy(() => import("@/layouts/auth/outlets/login"));
+const AuthSignupLayout = lazy(() => import("@/layouts/auth/outlets/signup"));
 /*** Layouts ***/
 
 /*** Routes ***/
+// Root
 const rootRoute = createRootRoute({
   component: () => (
     <>
@@ -21,10 +28,29 @@ const rootRoute = createRootRoute({
   ),
   notFoundComponent: NotFoundComponent,
 });
+
+// Auth
+const authRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: "auth",
+  component: AuthLayout,
+});
+const authLoginRoute = createRoute({
+  getParentRoute: () => authRoute,
+  path: "login",
+  component: AuthLoginLayout,
+});
+const authSignupRoute = createRoute({
+  getParentRoute: () => authRoute,
+  path: "signup",
+  component: AuthSignupLayout,
+});
 /*** Routes ***/
 
 // Creating route
-const routeTree = rootRoute.addChildren([]);
+const routeTree = rootRoute.addChildren([
+  authRoute.addChildren([authLoginRoute, authSignupRoute]),
+]);
 
 const router = createRouter({
   routeTree,
